b040a07a9a62af16cca101323ebff76b4a44bd85
ArmanTaheriGhaleTaki <armantahery1381@gmail.com>
2025-01-06 05:39:40 +0330

    chore(): add dns functions

---
+++ internal/dns/function.go
@@ -0,0 +1,28 @@
+package dns
+
+import (
+	"net/url"
+
+	"github.com/urfave/cli/v2"
+)
+
+func URLValidator(URL string) bool {
+	// Parse the URL
+	u, err := url.Parse(URL)
+	if err != nil {
+		return false
+	}
+	// Check if the scheme is either "http" or "https"
+	if u.Scheme != "http" && u.Scheme != "https" {
+		return false
+	}
+	// Check if the host is present
+	if u.Host == "" {
+		return false
+	}
+	return true
+}
+
+func CheckWithURL(c *cli.Context) error {
+	return nil
+}
